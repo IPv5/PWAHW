3d667682d236055f43d5a941c35970bafb3f9396
IPv5 <donationbuycraft@gmail>
2020-08-13 09:01:48 -0700

    Added mlab support

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+var MONGODB_URI =
+    process.env.MONGODB_URI || 'mongodb://localhost/PWAHW';
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
 });
 
 // routes
