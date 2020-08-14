f8d898b33c2d2190647872c6ec0da2ed49a5d7f7
IPv5 <donationbuycraft@gmail>
2020-08-14 11:03:44 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
-
-const PORT = 3000;
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(logger("dev"));
+app.use(morgan('dev'));
 
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 var MONGODB_URI =
-    process.env.MONGODB_URI || 'mongodb://localhost/PWAHW';
+    process.env.MONGODB_URI || 'mongodb://localhost/tranquil-ridge-18150';
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
 });
 
-// routes
-app.use(require("./routes/api.js"));
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+app.listen(PORT, function () {
+    console.log(`App listening on Port ${PORT}`);
 });
